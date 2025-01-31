95d57229732c4c0b54fc44bf4859fffc13c6e018
ivy <ivngigiy@gmail.com>
2025-01-31 16:00:34 +0300

    Add SQL script to create tables

--- task_2.sql
+++ task_2.sql
@@ -1,13 +1,13 @@
--- Ensure the database is selected
+- Ensure the database is selected
 USE alx_book_store;
 
--- Creating the Authors table
+
 CREATE TABLE IF NOT EXISTS Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
--- Creating the Books table
+
 CREATE TABLE IF NOT EXISTS Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
@@ -17,7 +17,7 @@ CREATE TABLE IF NOT EXISTS Books (
     FOREIGN KEY (author_id) REFERENCES Authors(author_id) ON DELETE SET NULL
 );
 
--- Creating the Customers table
+
 CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS Customers (
     address TEXT
 );
 
--- Creating the Orders table
+
 CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id) ON DELETE CASCADE
 );
 
--- Creating the Order_Details table
+
 CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
